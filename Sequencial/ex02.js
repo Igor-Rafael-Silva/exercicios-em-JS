1e02c4e43ec2d7bd5e16591ed143ba63b9d4cfb4
Igor-Rafael-Silva <igor.r.souza@edu.senai.br>
2026-04-22 13:48:41 -0300

    ex02.js

--- Sequencial/ex02.js
+++ Sequencial/ex02.js
@@ -1,16 +1,6 @@
-// Importa o módulo para leitura de dados no terminal
+
 const prompt = require('prompt-sync')();
-
 console.log("--- Conversor: Celsius para Fahrenheit ---");
-
-// 1. Entrada de Dados
-// Usamos Number() para garantir que o valor seja tratado como número e não texto
 let celsius = Number(prompt("Digite a temperatura em graus Celsius: "));
-
-// 2. Processamento
-// Seguindo a fórmula: F = C * 9/5 + 32
 let fahrenheit = (celsius * 9) / 5 + 32;
-
-// 3. Saída de Dados
-// Usamos Template Strings (com crases) para exibir o resultado
 console.log(`${celsius}°C convertido para Fahrenheit é: ${fahrenheit}°F`);
